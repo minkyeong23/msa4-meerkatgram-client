e9e9953c7ac875bc78e7000495b9fea0ecb5e412
minkyeong23 <choi777765@gmail.com>
2026-06-05 11:58:46 +0900

    260605 [dev] 회원가입 완료

--- src/pages/auth/Registration.vue
+++ src/pages/auth/Registration.vue
@@ -5,6 +5,7 @@ import MyInput from '../../components/input/MyInput.vue';
 import { useFileStore } from '../../store/file/useFileStore.js';
 import { useAuthStore } from '../../store/auth/useAuthStore.js';
 import { useRouter } from 'vue-router';
+import registrationValidator from '../../util/validator/domain/auth/registrationValidator.js';
 
 const router = useRouter();
 const fileStore = useFileStore();
@@ -21,6 +22,22 @@ const registrationData = reactive({
 });
 
 const handleSubmit = async () => {
+  // 유효성 검사
+  const validationList = [
+    registrationValidator.email(registrationData.email),
+    registrationValidator.password(registrationData.password),
+    registrationValidator.passwordChk(registrationData.password, registrationData.passwordChk),
+    registrationValidator.nick(registrationData.nick),
+    registrationValidator.profile(registrationData.profile),
+  ];
+
+  const errorList = validationList.filter(val => val);
+
+  if(errorList.length > 0) {
+    alert(errorList.join('\n'));
+    return;
+  }
+
   try {
     await authStore.registration(registrationData)
       alert("회원가입에 성공했습니다.");
@@ -56,9 +73,6 @@ const handleChangeProfile = async (e) => {
 
       selectedFile.value = file;
     }
-
-    selectedFile.value = file;
-
     // 파일 객체를 브라우저에서 접근 가능한 임시 URL로 변환
     preview.value = URL.createObjectURL(file);
   }
